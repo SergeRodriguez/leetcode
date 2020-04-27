86be78529255328f32842e2555f562d2c337373b
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-04-27 19:17:50 -0400

    add proposed leetcode solution

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -2,7 +2,8 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
+
+const longestCommonPrefix = function(strs) {
     if(strs.length === 0){
       return ""
     }
@@ -30,3 +31,21 @@ var longestCommonPrefix = function(strs) {
 
 console.log(longestCommonPrefix(["flower","flow","flight"]))
 console.log(longestCommonPrefix(["aca","cba"]))
+
+// Leetcode Algorithm Proposed solution 
+
+const longestCommonPrefixLeetcode = function(strs){
+  if (strs.length === 0) return "";
+    let prefix = strs[0];
+    for (i = 1; i < strs.length; i++)
+        while (strs[i].indexOf(prefix) != 0) {
+            console.log("prefix:", prefix)
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix.length === 0) return "";
+        }        
+    return prefix;
+
+}
+
+console.log(longestCommonPrefixLeetcode(["flower","flow","flight"]))
+console.log(longestCommonPrefixLeetcode(["aca","cba"]))
